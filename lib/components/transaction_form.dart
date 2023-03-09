861060649a902f9b5e26e1448c1b846f81588034
Alan Braulio <alan-braulio@hotmail.com>
2023-03-09 20:06:53 -0300

    creating modal transation form and creating default styles

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final Function(String, double) onSubmit;
 
 
   TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
+
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0;
+
+    if(title.isEmpty || value <= 0) {
+      return;
+    }
+    widget.onSubmit(title, value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,21 +35,20 @@ class TransactionForm extends StatelessWidget {
           children: [
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(labelText: 'Titulo'),
             ),
             TextField(
+              keyboardType: const TextInputType.numberWithOptions(decimal: true),
               controller: valueController,
               decoration: const InputDecoration(labelText: 'Valor (R\$)'),
+              onSubmitted: (_) => _submitForm(),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 TextButton(
-                  onPressed: () {
-                    final title = titleController.text;
-                    final value = double.tryParse(valueController.text) ?? 0;
-                    onSubmit(title, value);
-                  },
+                  onPressed: _submitForm,
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.purple,
                   ),
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,25 +11,28 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-          child: Column(
-        children: transactions.map((tr) {
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
           return Card(
             child: Row(
               children: [
                 Container(
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
                     decoration: BoxDecoration(
-                        border:
-                            Border.all(color: Colors.purple.shade50, width: 2)),
+                      border: Border.all(
+                          color: Theme.of(context).colorScheme.primary,
+                          width: 2),
+                    ),
                     padding: const EdgeInsets.all(10),
                     child: Text(
                       'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: const TextStyle(
+                      style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
-                        color: Colors.purple,
+                        color: Theme.of(context).colorScheme.primary,
                       ),
                     )),
                 Column(
@@ -53,8 +56,8 @@ class TransactionList extends StatelessWidget {
               ],
             ),
           );
-        }).toList(),
-      )),
+        },
+      ),
     );
   }
 }
